ce1f7ee8c75730c81775b172f996dc170e9657da
zer0 <leonardogomesk4@gmail.com>
2026-06-10 01:11:31 -0300

    Limpado arquivos padrão

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <HelloWorld />
+  <div class="editor-container">
+    <h1>JSON UI Builder</h1>
+  </div>
 </template>
+
+<style scoped>
+.editor-container {
+  width: 100vw;
+  height: 100vh;
+  background-color: #1e1e1e;
+  color: white;
+}
+</style>
